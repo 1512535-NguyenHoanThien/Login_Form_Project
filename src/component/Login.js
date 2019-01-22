e68a9f2afe3896f53556ac39d7a35082fcf728d4
Nguyễn Phương Nam <npnam.fit.hcmus@gmail.com>
2019-01-22 13:00:50 +0700

    fetch api

--- src/component/Login.js
+++ src/component/Login.js
@@ -4,14 +4,43 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './style.css';
 
 class App extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            username:'',
+            password:'',
+        }
+        this.onLogIn = this.onLogIn.bind(this)
+        this.onHandleChange = this.onHandleChange.bind(this)
+    }
+
+    onHandleChange = (e) => {
+        var target = e.target;
+        var name =target.name;
+        var value = target.value;
+        this.setState({
+            [name]: value
+        })
+        console.log(this.state);
+    }
+
+    onLogIn(){
+      fetch('localhost:4000/login', {
+         method: 'POST',      
+         body: {
+           
+        }
+  });
+    }
+
     render() {
         return (
             <div className="login-page">
                 <div className="form">
-                    <form className="login-form">
-                        <input type="text" placeholder="username" />
-                        <input type="password" placeholder="password" />
-                        <Link to="/profile"><button>login</button></Link>
+                    <form className="login-form" action='/login'>
+                        <input onChange={this.onHandleChange} type="text" placeholder="username" name="username" value={this.state.username}/>
+                        <input onChange={this.onHandleChange} type="password" placeholder="password" name="password" value={this.state.password}/>
+                        <button onClick={this.onLogIn}>login</button>
                         <p className="message">Not registered?
                             <Link to="/register">Create an account</Link>
                         </p>
